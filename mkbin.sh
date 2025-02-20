ffb578fae21e1b01382f07410b08a30909047e4a
Sandwhiched <sandwh1ched.real@gmail.com>
2025-02-20 17:45:16 -0600

    Some work I guess

--- mkbin.sh
+++ mkbin.sh
@@ -1,15 +1,11 @@
 # Script requirements:
 # - GraalVM
 
-if [[ "$1" == "-q" ]]; then
-    javac chunk/util/*.java
-    javac Chunk.java
-    native-image Chunk -o ./chunk.x86_64 -Ob
-elif [[ "$1" == "-c" ]]; then
-    javac chunk/util/*.java
+if [[ "$1" == "-c" ]]; then
+    javac chunk/*/*.java
     javac Chunk.java
 else
-    javac chunk/util/*.java
+    javac chunk/*/*.java
     javac Chunk.java
     native-image Chunk -o ./chunk.x86_64
 fi
